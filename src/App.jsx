df47b964700d03c459e91d61e7253615b040b658
Zahra_Ahmadnejad <z.ahmadnejadz@gmail.com>
2024-05-02 09:43:54 -0500

    dorost kardan safheie ke vaghti hich boardi vojod nadare miad

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import React, { useState } from 'react' 
-import Header from './Components/Header'
-import Center from './Components/Center'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Header from "./Components/Header";
+// import Home from "./Components/Home";
+import EmptyBoard from './Components/EmptyBoard';
+import boardsSlice from "./Redux/boardsSlice";
 
 function App() {
-
-  const [boardModalOpen, setBoardModalOpen] = useState(false) /// تعریف دو ورودی با مقدار اولیه ی فالس
-  return ( 
-  
-  <div>
-
-  {/* Header Section */}
-
-  <Header boardModalOpen = {boardModalOpen} setBoardModalOpen={setBoardModalOpen}/>
-
-
-  {/* Center Section */}
-  <Center/>
-
-
-</div>
-)
+  const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boards);
+  const activeBoard = boards.find((board) => board.isActive);
+  if (!activeBoard && boards.length > 0)
+    dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
+  return (
+    <div className=" overflow-hidden  overflow-x-scroll">
+      <>
+        {boards.length > 0 ?
+        <>
+        <Header
+          setIsBoardModalOpen={setIsBoardModalOpen}
+          isBoardModalOpen={isBoardModalOpen}
+        />
+        {/* <Home
+          setIsBoardModalOpen={setIsBoardModalOpen}
+          isBoardModalOpen={isBoardModalOpen}
+        /> */}
+        </>
+        :
+        <>
+          <EmptyBoard type='add'/>
+        </>
+      }
+        
+      </>
+    </div>
+  );
 }
 
-export default  App
+export default App;
